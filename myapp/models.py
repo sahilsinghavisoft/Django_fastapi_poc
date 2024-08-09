e331fab4286d46c3806d746bd78793eccd59c18b
sahilsinghavisoft <sahil.singh@avisoft.io>
2024-08-09 15:19:22 +0530

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import date
+
+class Student(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(default=None, blank=True, null=True)  # Allow null and blank
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
